da3da835be423711e5a1f7f79e69cf80f5d0ac54
CyberKuryber <urosstanic25@gmail.com>
2022-02-14 23:36:39 +0100

    imlemented wal

---
+++ Wal.go
@@ -0,0 +1,212 @@
+package main
+
+import (
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"strconv"
+)
+
+const (
+	THRESHOLD_DEFAULT        = 10
+	ENTRIES_PER_FILE_DEFAULT = 10
+	PATH                     = "wal/WAL"
+	LOW_WATERMARK            = 2
+)
+
+type Wal struct {
+	WalBuffer      []*WalEntry
+	threshold      int32
+	currentFile    int32
+	entriesPerFile int32
+	inCurrentFile  int32
+	lowWatermark   int
+	path           string
+}
+
+func newWal() *Wal {
+	threshold := THRESHOLD_DEFAULT
+	entriesPerFile := ENTRIES_PER_FILE_DEFAULT
+	lowWatermark := LOW_WATERMARK
+	path := PATH
+
+	//TODO read config
+
+	thresholdValid := false
+	entriesPerFileValid := false
+	if thresholdValid {
+		fmt.Println("T: Valid")
+	}
+	if entriesPerFileValid {
+		fmt.Println("EPF: Valid")
+	}
+
+	files, _ := ioutil.ReadDir("WAL/")
+	currentFile := len(files)
+
+	return &Wal{
+		WalBuffer:      make([]*WalEntry, 0),
+		threshold:      int32(threshold),
+		currentFile:    int32(currentFile),
+		entriesPerFile: int32(entriesPerFile),
+		inCurrentFile:  0,
+		lowWatermark:   lowWatermark,
+		path:           path,
+	}
+
+}
+
+func (wal *Wal) put(s string, data []byte) {
+	if int32(len(wal.WalBuffer)) >= wal.threshold {
+		wal.dump()
+
+	}
+	newEntry := newWalEntry()
+	newEntry.put(s, data)
+	wal.WalBuffer = append(wal.WalBuffer, newEntry)
+}
+
+func (wal *Wal) deleteSegments() {
+	files, _ := ioutil.ReadDir("WAL/")
+	fileCount := len(files)
+
+	if fileCount > wal.lowWatermark {
+		i := 0
+		for i = 0; i < fileCount-wal.lowWatermark; i++ {
+			os.Remove(wal.path + strconv.Itoa(i) + ".gob")
+		}
+		for ; i < fileCount; i++ {
+			os.Rename(wal.path+strconv.Itoa(i)+".gob",
+				wal.path+strconv.Itoa(i-(fileCount-wal.lowWatermark))+".gob")
+			fmt.Println(i - (fileCount - wal.lowWatermark))
+		}
+	}
+
+}
+
+func (wal *Wal) dump() bool {
+	appendFile, _ := os.OpenFile(PATH+strconv.Itoa(int(wal.currentFile))+".gob", os.O_RDWR|os.O_CREATE, 0644)
+
+	for i := 0; i < len(wal.WalBuffer); i++ {
+		wal.WalBuffer[i].prepareDump()
+		err := appendToFile(appendFile, wal.WalBuffer[i].toBytes())
+		if err != nil {
+			return false
+		}
+		wal.inCurrentFile++
+		if wal.inCurrentFile >= wal.entriesPerFile {
+			wal.currentFile++
+			appendFile, _ = os.OpenFile(PATH+strconv.Itoa(int(wal.currentFile))+".gob", os.O_RDWR|os.O_CREATE, 0644)
+		}
+	}
+	wal.inCurrentFile = 0
+	wal.WalBuffer = make([]*WalEntry, 0)
+	appendFile.Close()
+	return true
+}
+
+func appendToFile(file *os.File, data []byte) error {
+
+	fmt.Println(data)
+	file.Seek(0, 2)
+	file.Write(data)
+
+	return nil
+}
+
+func (wal *Wal) recovery() {
+
+	files, _ := ioutil.ReadDir("WAL/")
+	fileCount := len(files)
+
+	for i := 0; i < fileCount; i++ {
+		fileName := "WAL/WAL" + strconv.Itoa(i) + ".gob"
+		source, _ := os.OpenFile(fileName, os.O_RDONLY, 0777)
+		fileL, _ := fileLen(source)
+		if fileL == 0 {
+			break
+		}
+		currentFileIndex := 0
+		for j := 0; true; j++ {
+
+			bs, err := readRange(source, currentFileIndex, currentFileIndex+21)
+			currentFileIndex += 21
+
+			if err == io.EOF {
+				source.Close()
+				break
+			}
+
+			crc := binary.LittleEndian.Uint32(bs[:4])
+
+			seed := binary.LittleEndian.Uint64(bs[4:12])
+
+			tombstone := bs[12:13]
+
+			keySize := binary.LittleEndian.Uint32(bs[13:17])
+
+			valueSize := binary.LittleEndian.Uint32(bs[17:21])
+
+			bs, _ = readRange(source, currentFileIndex, currentFileIndex+int(keySize))
+			currentFileIndex += int(keySize)
+			key := bs
+
+			bs, _ = readRange(source, currentFileIndex, currentFileIndex+int(valueSize))
+			currentFileIndex += int(valueSize)
+			value := bs
+
+			currentEntrie := WalEntry{
+				Crc:       crc,
+				Seed:      seed,
+				Tombstone: tombstone[0],
+				KeySize:   keySize,
+				ValueSize: valueSize,
+				Key:       key,
+				Value:     value,
+			}
+
+			currentEntrie.checkValidity()
+		}
+
+	}
+
+}
+
+func readRange(file *os.File, startIndex, endIndex int) ([]byte, error) {
+	if startIndex < 0 || endIndex < 0 || startIndex > endIndex {
+		return nil, errors.New("indices invalid")
+	}
+
+	result := make([]byte, endIndex-startIndex)
+	fmt.Println(endIndex - startIndex)
+	_, err := io.ReadFull(file, result)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func fileLen(file *os.File) (int64, error) {
+	info, err := file.Stat()
+	if err != nil {
+		return 0, err
+	}
+	return info.Size(), nil
+}
+
+func WalTest() {
+	wal := newWal()
+	for i := 0; i < 8; i++ {
+		for j := 0; j < 10; j++ {
+			bs := make([]byte, 4)
+			binary.LittleEndian.PutUint32(bs, uint32(j))
+			//fmt.Println(bs)
+			wal.put(strconv.Itoa(i), bs)
+		}
+	}
+	wal.deleteSegments()
+	wal.recovery()
+}
---
+++ WalEntry.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"encoding/binary"
+	_ "fmt"
+	_ "hash"
+	"hash/crc32"
+	_ "math"
+	_ "sort"
+	"time"
+	_ "time"
+	_ "unsafe"
+)
+
+type WalEntry struct {
+	Crc       uint32
+	Seed      uint64
+	Tombstone byte //deleted = 1
+	KeySize   uint32
+	ValueSize uint32
+	Key       []byte
+	Value     []byte
+}
+
+/*
+   +---------------+-----------------+---------------+---------------+-----------------+-...-+--...--+
+   |    CRC (4B)   | Timestamp (16B) | Tombstone(1B) | Key Size (8B) | Value Size (8B) | Key | Value |
+   +---------------+-----------------+---------------+---------------+-----------------+-...-+--...--+
+   CRC = 32bit hash computed over the payload using CRC
+   Key Size = Length of the Key data
+   Tombstone = If this record was deleted and has a value
+   Value Size = Length of the Value data
+   Key = Key data
+   Value = Value data
+   Timestamp = Timestamp of the operation in seconds
+*/
+
+const (
+	T_SIZE = 8
+	C_SIZE = 4
+
+	CRC_SIZE       = T_SIZE + C_SIZE
+	TOMBSTONE_SIZE = CRC_SIZE + 1
+	KEY_SIZE       = TOMBSTONE_SIZE + T_SIZE
+	VALUE_SIZE     = KEY_SIZE + T_SIZE
+)
+
+func CRC32(data []byte) uint32 {
+	return crc32.ChecksumIEEE(data)
+}
+
+func newWalEntry() *WalEntry {
+	entry := WalEntry{
+		Crc:       0,
+		Seed:      uint64(time.Now().Unix()),
+		Tombstone: 0, //1 if deleted
+		KeySize:   0,
+		ValueSize: 0,
+		Key:       nil,
+		Value:     nil,
+	}
+	return &entry
+}
+
+func (walEntry *WalEntry) put(key string, value []byte) {
+	walEntry.Key = []byte(key)
+	walEntry.Value = value
+	walEntry.KeySize = uint32(len(walEntry.Key))
+	walEntry.ValueSize = uint32(len(walEntry.Value))
+}
+
+func (walEntry *WalEntry) prepareDump() {
+	walEntry.Crc = CRC32(walEntry.toBytes())
+}
+
+func (walEntry *WalEntry) checkValidity() bool { // if valid returns true
+	readCrc := walEntry.Crc
+	walEntry.Crc = 0
+	walEntry.prepareDump()
+	return readCrc == walEntry.Crc
+}
+
+func (walEntry *WalEntry) toBytes() []byte {
+	allBytes := make([]byte, 0)
+	bs := make([]byte, 4)
+	binary.LittleEndian.PutUint32(bs, walEntry.Crc)
+	allBytes = append(allBytes, bs...)
+
+	bs = make([]byte, 8)
+	binary.LittleEndian.PutUint64(bs, walEntry.Seed)
+	allBytes = append(allBytes, bs...)
+
+	allBytes = append(allBytes, walEntry.Tombstone)
+
+	bs = make([]byte, 4)
+	binary.LittleEndian.PutUint32(bs, walEntry.KeySize)
+	allBytes = append(allBytes, bs...)
+
+	bs = make([]byte, 4)
+	binary.LittleEndian.PutUint32(bs, walEntry.ValueSize)
+	allBytes = append(allBytes, bs...)
+
+	allBytes = append(allBytes, walEntry.Key...)
+	allBytes = append(allBytes, walEntry.Value...)
+
+	return allBytes
+}
