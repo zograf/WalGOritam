74b37a4929e5ef06cc74fb55689ae02203e00542
zograf <lazzarmilanovic@gmail.com>
2022-02-16 01:05:51 +0100

    main input loop

--- main.go
+++ main.go
@@ -1,7 +1,49 @@
 package main
 
-import src "github.com/zograf/WalGOritam/src"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	src "github.com/zograf/WalGOritam/src"
+)
+
+func help() {
+	fmt.Println("Usage: PUT <key> <value>")
+	fmt.Println("       GET <key>")
+	fmt.Println("       DEL <key>")
+	fmt.Println("")
+	fmt.Println("Quit:  EXIT")
+}
 
 func main() {
 	src.TestCache()
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("> ")
+		text, _ := reader.ReadString('\n')
+		text = strings.ReplaceAll(text, "\n", "")
+		if text == "EXIT" {
+			os.Exit(0)
+		}
+		tokens := strings.Split(text, " ")
+		if tokens[0] == "PUT" {
+			if len(tokens) == 3 {
+				src.EnginePut(tokens[1], tokens[2])
+				continue
+			}
+		} else if tokens[0] == "GET" {
+			if len(tokens) == 2 {
+				src.EngineGet(tokens[1])
+				continue
+			}
+		} else if tokens[0] == "DEL" {
+			if len(tokens) == 2 {
+				src.EngineDelete(tokens[1])
+				continue
+			}
+		}
+		help()
+	}
 }
---
+++ src/Engine.go
@@ -0,0 +1,16 @@
+package src
+
+import "fmt"
+
+func EnginePut(key, value string) {
+	fmt.Println("PUT")
+}
+
+func EngineGet(key string) {
+	fmt.Println("GET")
+}
+
+func EngineDelete(key string) {
+	fmt.Println("DEL")
+
+}
