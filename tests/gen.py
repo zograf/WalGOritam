0be9b5493bdc4834fa5e99c4258219511f32d6b5
CyberKuryber <urosstanic25@gmail.com>
2022-02-22 16:00:24 +0100

    Better testing script

--- tests/gen.py
+++ tests/gen.py
@@ -4,11 +4,17 @@ file_name = "datoteka.txt"
 # Number of records to generate
 records = int(sys.argv[1])
 with open(file_name, "w") as f:
+    generated_keys = []
     for _ in range(records):
-        csv_code = ''.join(random.choices(string.ascii_letters, k=8))
-        csv_id = random.randint(100, 1000)
-        # csv_date = "28212021123321"
-        # csv_cell = ''.join(random.choices(string.ascii_uppercase + string.digits, k = 5))
-        # csv_sentence = random.randint(0, 480)
-        # csv_deleted = "False"
-        f.write("PUT" + " " + csv_code + " " + str(csv_id) + "\n")
+        key = ''.join(random.choices(string.ascii_letters, k=8))
+        generated_keys.append(key)
+        value = random.randint(100, 1000)
+        f.write("PUT" + " " + key + " " + str(value) + "\n")
+
+    for _ in range(records // 2):
+        key = generated_keys[random.randint(0, len(generated_keys) - 1)]
+        toss = random.randint(0, 1)
+        if toss == 1:
+            f.write("DEL" + " " + key + "\n")
+        else:
+            f.write("GET" + " " + key + "\n")
